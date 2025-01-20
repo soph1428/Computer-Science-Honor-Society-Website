8fe14a2393304871e1eaf349c62fd8aa81cfbe3d
soph1428 <sophiagroth16@gmail.com>
2025-01-20 17:43:39 -0600

    update

---
+++ script.js
@@ -0,0 +1,28 @@
+let memberButton = document.getElementById(`member`)
+let information = document.getElementById(`information`)
+let events = document.getElementById(`events`)
+let eventsContainer = document.getElementById(`eventsContainer`)
+let currentEvents = [{what: `Computer Science Carnival`, where: `Olathe South Commons`, when: `February 1st, : to :`}]
+
+function member() {
+    information.style.display = `inline-flex`
+    events.style.display = `unset`
+    eventsContainer.innerHTML = ``
+    for (let currentEvent of currentEvents) {
+        let eventName = document.createElement(`strong`)
+        eventName.style.fontSize = `min(3vw, 4vh)`
+        eventName.textContent = currentEvent.what
+        let eventLocation = document.createElement(`label`)
+        eventLocation.style.fontSize = `min(3vw, 4vh)`
+        eventLocation.textContent = currentEvent.where
+        let eventTime = document.createElement(`label`)
+        eventTime.style.fontSize = `min(3vw, 4vh)`
+        eventTime.textContent = currentEvent.when
+        eventsContainer.append(eventName, document.createElement(`br`), eventLocation, document.createElement(`br`), eventTime)
+    }
+}
+
+function nonMember() {
+    information.style.display = `inline-flex`
+    events.style.display = `none`
+}
