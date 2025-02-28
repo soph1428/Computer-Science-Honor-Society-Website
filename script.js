ad22f5e3eff0d8f590100764c37e572e1421a096
soph1428 <sophiagroth16@gmail.com>
2025-02-28 00:24:48 -0600

    update

--- script.js
+++ script.js
@@ -6,34 +6,36 @@ let noEvents = document.getElementById(`noEvents`)
 let moreInformation = document.getElementById(`moreInformation`)
 let eligibilityText = document.getElementById(`eligibilityText`)
 let applicationLink = document.getElementById(`applicationLink`)
-let currentEvents = []
+let currentEvents = [{what: `8th Grade Activity Fair`, where: `Olathe South Aux Gym`, when: `March 4th, 10:10 AM - 12:35 PM<br>Sophia Groth and Miles Knapp`}]
 
 function member() {
     information.style.display = `inline-flex`
-    events.style.display = `unset`
-    moreInformation.style.paddingLeft = `min(3.75vw, 5vh)`
-    eventsContainer.innerHTML = ``
-    applicationLink.hidden = true
+    document.body.querySelectorAll(`*`).forEach((elem) => {
+        if (elem.className == `member`) {
+            elem.style.display = `unset`
+        } if (elem.className == `nonmember` || elem.id == noEvents.id) elem.style.display = `none`
+    }); eventsContainer.innerHTML = ``
     eligibilityText.textContent = eligibilityText.textContent.replaceAll(`Members`, `You`)
     if (currentEvents.length == 0) noEvents.style.display = `unset`
     for (let currentEvent of currentEvents) {
         let eventName = document.createElement(`strong`)
         eventName.style.fontSize = `min(3vw, 4vh)`
-        eventName.textContent = currentEvent.what
+        eventName.innerHTML = currentEvent.what
         let eventLocation = document.createElement(`label`)
         eventLocation.style.fontSize = `min(3vw, 4vh)`
-        eventLocation.textContent = currentEvent.where
+        eventLocation.innerHTML = currentEvent.where
         let eventTime = document.createElement(`label`)
         eventTime.style.fontSize = `min(3vw, 4vh)`
-        eventTime.textContent = currentEvent.when
-        eventsContainer.append(eventName, document.createElement(`br`), eventLocation, document.createElement(`br`), eventTime)
+        eventTime.innerHTML = currentEvent.when
+        eventsContainer.append(document.createElement(`br`), eventName, document.createElement(`br`), eventLocation, document.createElement(`br`), eventTime)
     }
 }
 
 function nonMember() {
     information.style.display = `inline-flex`
-    events.style.display = `none`
-    moreInformation.style.paddingLeft = ``
-    applicationLink.hidden = false
-    eligibilityText.textContent = eligibilityText.textContent.replaceAll(`You`, `Members`)
+    document.body.querySelectorAll(`*`).forEach((elem) => {
+        if (elem.className == `member`) {
+            elem.style.display = `none`
+        } else if (elem.tagName != `SCRIPT` && elem.id != eventsContainer.id) elem.style.display = `unset`
+    }); eligibilityText.textContent = eligibilityText.textContent.replaceAll(`You`, `Members`)
 }
